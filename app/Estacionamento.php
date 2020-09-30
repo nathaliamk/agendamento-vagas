ff019f313b05d92ff8ea534d54a648034ebfb9b9
Guh.Snt.Lean <guleandro.66@gmail.com>
2020-09-30 09:26:20 -0300

    Ajustando VagasController

--- app/Estacionamento.php
+++ app/Estacionamento.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Estacionamento extends Model
 {
     protected $table = "estacionamento";
-    // protected $fillable = ['','','',''];
+    protected $fillable = ['total_vagas','vagas_tipo1','vagas_tipo2','vagas_tipo3'];
 }
